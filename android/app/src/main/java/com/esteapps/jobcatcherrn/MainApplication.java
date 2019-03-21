0f2e97acb62d3ce80cb77032b9f7e905241a0c16
Edward Stevens <edward@jobcatcher.io>
2019-03-21 16:16:20 +0000

    Added react-native-google-signin for Android

--- android/app/src/main/java/com/esteapps/jobcatcherrn/MainApplication.java
+++ android/app/src/main/java/com/esteapps/jobcatcherrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.esteapps.jobcatcherrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
             new RNGestureHandlerPackage(),
           new RNFirebasePackage(),
           new RNFirebaseFirestorePackage(),
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
+import {Alert, ScrollView} from 'react-native';
 import {Button, Text, TextField, View} from 'react-native-ui-lib';
 import {connect} from 'react-redux';
 import firebase from 'react-native-firebase';
+import {GoogleSignin, GoogleSigninButton, statusCodes} from 'react-native-google-signin';
 
 const mapStateToProps = state => {
   return {};
@@ -38,6 +39,65 @@ class SignInScreenRedux extends React.Component {
       });
   }
 
+  /**
+   * When the App component mounts, we listen for any authentication
+   * state changes in Firebase.
+   * Once subscribed, the 'user' parameter will either be null
+   * (logged out) or an Object (logged in)
+   */
+  componentDidMount() {
+    this._configureGoogleSignIn();
+    //await this._getCurrentUser();
+  }
+
+  _configureGoogleSignIn() {
+    GoogleSignin.configure();
+  }
+
+  _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn()
+        .then((data) => {
+          // Create a new Firebase credential with the token
+          const credential = firebase.auth.GoogleAuthProvider.credential(data.idToken, data.accessToken);
+          // Login with the credential
+          return firebase.auth().signInWithCredential(credential).then(
+            () => {
+              this.props.navigation.navigate('JobApplicationListScreen')
+            });
+        }).then((user) => {
+          // If you need to do anything with the user, do it here
+          // The user will be logged in automatically by the
+          // `onAuthStateChanged` listener we set up in App.js earlier
+        })
+        .catch((error) => {
+          const {code, message} = error;
+          // For details of error codes, see the docs
+          // The message contains the default Firebase string
+          // representation of the error
+        });
+
+      ;
+      //this.setState({userInfo, error: null});
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // sign in was cancelled
+        Alert.alert('cancelled');
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation in progress already
+        Alert.alert('in progress');
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        Alert.alert('play services not available or outdated');
+      } else {
+        Alert.alert('Something went wrong', error.toString());
+        this.setState({
+          error,
+        });
+      }
+    }
+  };
+
   render() {
 
     return (
@@ -71,7 +131,14 @@ class SignInScreenRedux extends React.Component {
             <View marginT-100 center>
               <Button text70 white background-orange30 label="Login" onPress={this.onLogin}/>
               <Button link text70 orange30 label="Sign Up" marginT-20/>
+              <GoogleSigninButton
+                style={{width: 212, height: 48}}
+                size={GoogleSigninButton.Size.Standard}
+                color={GoogleSigninButton.Color.Auto}
+                onPress={this._signIn}
+              />
             </View>
+
           </View>
 
           {/*          <View style={{flexDirection: 'row'}}>
