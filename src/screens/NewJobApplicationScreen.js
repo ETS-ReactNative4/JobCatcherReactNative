15e00b86d0010a9da85f9acd49a6fd3ea8d9ba02
Edward Stevens <edward@jobcatcher.io>
2019-04-10 19:57:48 +0100

    Added additional fields

--- src/screens/NewJobApplicationScreen.js
+++ src/screens/NewJobApplicationScreen.js
@@ -1,24 +1,18 @@
 import React, {Fragment} from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
-import {
-  BorderRadiuses,
-  Button,
-  Carousel,
-  Colors,
-  Constants,
-  ListItem,
-  LoaderScreen,
-  TabBar,
-  TextField,
-  ThemeManager
-} from 'react-native-ui-lib';
+import {Alert, ScrollView, StyleSheet} from 'react-native';
+import {Button, Card, Colors, Constants, ListItem, Picker, Text, TextField, View} from 'react-native-ui-lib';
 import {connect} from 'react-redux';
+import CustomColours from "../constants/CustomColours";
+import Icon from 'react-native-ionicons'
+import {CONTRACT_TIME, CONTRACT_TYPE, WAGE_CURRENCY, WAGE_FREQUENCY} from "../utils/Arrays";
+import _ from 'lodash';
+
 
 const mapStateToProps = state => {
   return {};
 };
 
-class JobApplicationDetailRedux extends React.Component {
+class NewJobApplicationRedux extends React.Component {
   static navigationOptions = {
     title: 'New Job Application',
   };
@@ -29,7 +23,12 @@ class JobApplicationDetailRedux extends React.Component {
       showLoader: true,
       jobApplication: '',
       selectedIndex: 0,
-      currentPage: 0
+      currentPage: 0,
+      wageCurrency: WAGE_CURRENCY[0].value,
+      wageAmount: 0,
+      wageFrequency: WAGE_FREQUENCY[4].value,
+      contractType: CONTRACT_TYPE[2].value,
+      contractTime: CONTRACT_TIME[0].value
     };
 
   }
@@ -39,24 +38,245 @@ class JobApplicationDetailRedux extends React.Component {
     return (
       <Fragment>
         <ScrollView style={styles.container} keyboardShouldPersistTaps="always">
-          <TextField
-            text50
-            dark10
-            placeholder='Job Title'
-            textContentType='jobTitle'
-            keyboardType='default'
-            autoCapitalize='words'
-            onChangeText={(text) => this.setState({jobTitle: text})}
-            value={this.state.jobTitle}/>
-          <TextField
-            text50
-            dark10
-            placeholder='Company Name'
-            textContentType='organizationName'
-            keyboardType='default'
-            autoCapitalize='words'
-            onChangeText={(text) => this.setState({companyName: text})}
-            value={this.state.companyName}/>
+          <View
+            style={{paddingTop: 24, paddingRight: 24, paddingLeft: 24}}>
+            <TextField
+              text50
+              style={{color: CustomColours.colorPrimary}}
+              placeholder='Job Title'
+              textContentType='jobTitle'
+              autoCapitalize='words'
+              onChangeText={(text) => this.setState({jobTitle: text})}
+              value={this.state.jobTitle}/>
+            <TextField
+              text50
+              style={{color: CustomColours.colorPrimary}}
+              placeholder='Company Name'
+              textContentType='organizationName'
+              autoCapitalize='words'
+              onChangeText={(text) => this.setState({companyName: text})}
+              value={this.state.companyName}/>
+          </View>
+          <ListItem
+            activeBackgroundColor={Colors.dark60}
+            activeOpacity={0.3}
+            onPress={() => {
+              Alert.alert('Location', 'Add Location here.');
+            }}
+            containerStyle={{padding: 8}}>
+            <ListItem.Part left style={styles.image}>
+              <Icon
+                name='pin'
+                size={26}
+                color={CustomColours.colorPrimary}
+              />
+            </ListItem.Part>
+            <ListItem.Part middle column>
+              <ListItem.Part>
+                <Text dark10 text70 numberOfLines={1}>Location</Text>
+              </ListItem.Part>
+              <ListItem.Part>
+                <Text text70 dark40 numberOfLines={1}>Add Location</Text>
+              </ListItem.Part>
+            </ListItem.Part>
+          </ListItem>
+          <Card style={styles.card}
+                onPress={() => {
+                }}>
+            <View style={{margin: 16}}>
+              <View
+                style={{alignItems: 'center', justifyContent: 'center'}}
+                onPress={() => {
+                }}>
+                <Text text70 dark10>
+                  Wishlist
+                </Text>
+              </View>
+              <Button
+                backgroundColor={CustomColours.colorPrimary}
+                color={CustomColours.colorOnPrimary}
+                label="Start Application"
+                labelStyle={{fontWeight: '600'}}
+                style={{marginTop: 8, borderRadius: 3}}
+                onPress={() => {
+                  Alert.alert('New Application Stage', 'Add First Application Stage here.');
+                }}
+              />
+            </View>
+          </Card>
+
+          <View style={{marginLeft: 16, marginRight: 16}}>
+
+            <Button
+              backgroundColor={CustomColours.colorPrimary}
+              color={CustomColours.colorOnPrimary}
+              label='Set Deadline'
+              labelStyle={{fontWeight: '600'}}
+              style={{marginTop: 8, borderRadius: 3}}
+              iconSource={() => <Icon name='calendar' color={CustomColours.colorOnPrimary} style={{marginRight: 8}}/>}
+              onPress={() => {
+                Alert.alert('Set Deadline', 'Set a Deadline here.');
+              }}
+            />
+
+            <Button
+              outline
+              /*            backgroundColor={CustomColours.colorPrimary}*/
+              color={CustomColours.colorPrimary}
+              label='Add Reminder(s)'
+              labelStyle={{fontWeight: '600'}}
+              style={{marginTop: 8, borderRadius: 3}}
+              iconSource={() => <Icon name='alarm' color={CustomColours.colorPrimary} style={{marginRight: 8}}/>}
+              onPress={() => {
+                Alert.alert('Set Reminder(s)', 'Set a Deadline here.');
+              }}
+            />
+
+          </View>
+
+          <View
+            style={{paddingTop: 24, paddingRight: 24, paddingLeft: 24}}>
+            <Text dark10 text70 numberOfLines={1}>Wages</Text>
+          </View>
+          <View style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            alignItems: 'flex-start',
+            marginTop: 8
+          }}>
+
+            <View style={{flex: 1}}>
+              <Picker
+                placeholder={this.state.wageCurrency}
+                text50
+                hideUnderline
+                value={this.state.wageCurrency}
+                enableModalBlur={false}
+                onChange={item => this.setState({wageCurrency: item.value})}
+                topBarProps={{title: 'Wage Currency'}}
+                style={{color: Colors.red20, textAlign: 'center'}}>
+                {WAGE_CURRENCY.map(option => (
+                  <Picker.Item key={option.value} value={option} disabled={option.disabled} label={option.label}/>
+                ))}
+              </Picker>
+            </View>
+
+            <View style={{flex: 4}}>
+              <TextField
+                text50
+                style={{color: CustomColours.colorPrimary, paddingTop: 1}}
+                placeholder='0'
+                keyboardType='numeric'
+                onChangeText={(text) => this.setState({wageAmount: text})}
+                value={this.state.wageAmount ? String(this.state.wageAmount) : null}/>
+            </View>
+
+            <View style={{flex: 2}}>
+              <Picker
+                placeholder={this.state.wageFrequency}
+                text50
+                hideUnderline
+                value={this.state.wageFrequency}
+                enableModalBlur={false}
+                onChange={item => this.setState({wageFrequency: item.value})}
+                topBarProps={{title: 'Wage Frequency'}}
+                style={{color: Colors.red20, textAlign: 'center'}}>
+                {WAGE_FREQUENCY.map(option => (
+                  <Picker.Item key={option.value} value={option} disabled={option.disabled} label={option.label}/>
+                ))}
+              </Picker>
+            </View>
+
+          </View>
+
+          <View
+            style={{paddingTop: 16, paddingRight: 24, paddingLeft: 24}}>
+            <Text dark10 text70 numberOfLines={1}>Contract Type and Time</Text>
+          </View>
+          <View style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            alignItems: 'flex-start',
+            marginTop: 8
+          }}>
+
+            <View style={{flex: 2}}>
+              <Picker
+                placeholder={this.state.contractTime}
+                text50
+                hideUnderline
+                value={this.state.contractTime}
+                enableModalBlur={false}
+                onChange={item => this.setState({contractTime: item.value})}
+                topBarProps={{title: 'Contract Type'}}
+                style={{color: Colors.red20, paddingLeft: 32}}>
+                {CONTRACT_TYPE.map(option => (
+                  <Picker.Item key={option.value} value={option} disabled={option.disabled} label={option.label}/>
+                ))}
+              </Picker>
+            </View>
+
+            <View style={{flex: 1}}>
+              <TextField
+                text50
+                style={{color: CustomColours.colorPrimary, paddingTop: 1}}
+                placeholder='0'
+                keyboardType='numeric'
+                onChangeText={(text) => this.setState({wageAmount: text})}
+                value={this.state.wageAmount ? String(this.state.wageAmount) : null}/>
+            </View>
+            <Text dark10 text70 numberOfLines={1}>Hours</Text>
+
+          </View>
+
+          <View style={{flex: 1}}>
+            <Picker
+              placeholder={this.state.contractType}
+              text50
+              hideUnderline
+              value={this.state.contractType}
+              enableModalBlur={false}
+              onChange={item => this.setState({contractType: item.value})}
+              topBarProps={{title: 'Contract Type'}}
+              style={{color: Colors.red20, textAlign: 'center'}}>
+              {CONTRACT_TYPE.map(option => (
+                <Picker.Item key={option.value} value={option} disabled={option.disabled} label={option.label}/>
+              ))}
+            </Picker>
+          </View>
+          <View style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+          }}>
+            <Button
+              backgroundColor={CustomColours.colorPrimary}
+              color={CustomColours.colorOnPrimary}
+              label='Set Deadline'
+              labelStyle={{fontWeight: '600'}}
+              style={{marginTop: 8, borderRadius: 3}}
+              iconSource={() => <Icon name='calendar' color={CustomColours.colorOnPrimary} style={{marginRight: 8}}/>}
+              onPress={() => {
+                Alert.alert('Set Deadline', 'Set a Deadline here.');
+              }}
+            />
+            <Button
+              outline
+              backgroundColor={CustomColours.colorPrimary}
+              color={CustomColours.colorOnPrimary}
+              label='Add Reminder(s)'
+              labelStyle={{fontWeight: '600'}}
+              style={{marginTop: 8, borderRadius: 3}}
+              iconSource={() => <Icon name='alarm' color={CustomColours.colorOnPrimary} style={{marginRight: 8}}/>}
+              onPress={() => {
+                Alert.alert('Add Reminders', 'Add Reminders here.');
+              }}
+            />
+          </View>
+
+
         </ScrollView>
 
       </Fragment>
@@ -71,12 +291,18 @@ const styles = StyleSheet.create({
   },
   page: {
     width: Constants.screenWidth
-  }
+  },
+  image: {
+    marginRight: 16,
+    marginLeft: 8,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 });
 
-const JobApplicationDetailScreen = connect(
+const NewJobApplicationScreen = connect(
   mapStateToProps,
   null
-)(JobApplicationDetailRedux);
+)(NewJobApplicationRedux);
 
-export default JobApplicationDetailScreen;
+export default NewJobApplicationScreen;
