26407bfff163ecc1694cbbc20ec51d2973f3d333
Edward Stevens <edward@jobcatcher.io>
2019-04-10 19:34:51 +0100

    Added Arrays

---
+++ src/utils/Arrays.js
@@ -0,0 +1,44 @@
+//TODO: Confirm this array is correct
+export const APPLICATION_STAGE_ARRAY = [
+  {key: 'CV/Application Form', label: 'CV/Application Form', value: 'CV/Application Form'},
+  {key: 'Tests and Assessments', label: 'Tests and Assessments', value: 'Tests and Assessments'},
+  {key: 'Interview', label: 'Interview', value: 'Interview'},
+  {key: 'Complete', label: 'Complete', value: 'Complete'}
+];
+
+//TODO: Confirm this array is correct
+export const APPLICATION_STAGE_STATUS_ARRAY = [
+  {key: 'In Progress', label: 'In Progress', value: 'In Progress'},
+  {key: 'Successful', label: 'Successful', value: 'Successful'},
+  {key: 'Unsuccessful', label: 'Unsuccessful', value: 'Unsuccessful'},
+  {key: 'Withdrawn', label: 'Withdrawn', value: 'Withdrawn'}
+];
+
+export const WAGE_CURRENCY = [
+  {key: '£', label: '£', value: '£'},
+  {key: '$', label: '$', value: '$'},
+  {key: '€', label: '€', value: '€'},
+];
+export const WAGE_FREQUENCY = [
+  {key: 'Hourly', label: 'Hourly', value: 'Hourly'},
+  {key: 'Daily', label: 'Daily', value: 'Daily'},
+  {key: 'Weekly', label: 'Weekly', value: 'Weekly'},
+  {key: 'Bi-Weekly', label: 'Bi-Weekly', value: 'Bi-Weekly'},
+  {key: 'Monthly', label: 'Monthly', value: 'Monthly'},
+  {key: 'Annually', label: 'Annually', value: 'Annually'},
+  {key: 'Lump Sum', label: 'Lump Sum', value: 'Lump Sum'},
+  {key: 'Other', label: 'Other', value: 'Other'}
+];
+
+export const CONTRACT_TYPE = [
+  {key: 'Graduate Scheme', label: 'Graduate Scheme', value: 'Graduate Scheme'},
+  {key: 'Internship/Placement', label: 'Internship/Placement', value: 'Internship/Placement'},
+  {key: 'Permanent', label: 'Permanent', value: 'Permanent'},
+  {key: 'Temporary/Contract', label: 'Temporary/Contract', value: 'Temporary/Contract'},
+  {key: 'Other', label: 'Other', value: 'Other'}
+];
+export const CONTRACT_TIME = [
+  {key: 'Full Time', label: 'Full Time', value: 'Full Time'},
+  {key: 'Part Time', label: 'Part Time', value: 'Part Time'},
+  {key: 'Other', label: 'Other', value: 'Other'}
+];
